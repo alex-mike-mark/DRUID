81eceb90d2e0641aa00a9b168048b7456665f8e2
alex.mike.mark <alex.mike.mark@gmail.com>
2019-01-18 18:53:30 -0600

    Started up. Some basic movement.

---
+++ Assets/Scripts/DruidController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DruidController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    } 
+
+    void Update()
+    {
+        //get input loop
+        float h = Input.GetAxis("Horizontal");
+        bool x = Input.GetButton("Fire1");
+        bool y = Input.GetButton("Fire2");
+
+        // avoiding using addForce in order to create snappier movement.
+        if( x && h!=0f ){
+            rb.velocity = ( new Vector3(1,0,0) )*15*h;
+        } else if ( h!=0f ){
+            rb.velocity = ( new Vector3(1,0,0) )*10*h;
+        } else if ( x ){
+            Debug.Log(x);
+        }
+
+        if ( y ){
+
+        }
+    }
+
+    void Transform(){
+
+    }
+
+    void GetHit(){
+
+    }
+}
